2c3c064a04dac6bf31570eb85917b8e6155a0672
Qwertayguy <71943921+Qwertayguy@users.noreply.github.com>
2023-10-24 21:53:12 +0200

    Support

---
+++ Popular game clone/Assets/Scripts/DamageIsGay.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DamageIsGay : MonoBehaviour
+{
+    public int Health = 100;
+    private bool IsColliding = false;
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            IsColliding = true;
+        }
+    }
+    void Update()
+    {
+        if (IsColliding == true)
+        {
+            Health = Health - 5;
+        }
+        if (Health < 1)
+        {
+            Debug.Log("Skill issue");
+        }
+    }
+
+
+
+}
--- Popular game clone/Assets/Scripts/DoorOpen.cs
+++ Popular game clone/Assets/Scripts/DoorOpen.cs
@@ -16,7 +16,7 @@ public class DoorOpen : MonoBehaviour
     
     }
 
-    private void OnTriggerEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Door"))
         {
